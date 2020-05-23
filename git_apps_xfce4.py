08e40761ebdcf39458281710286dd7f57c95964c
kevinbowen777 <kevin.bowen@gmail.com>
2020-05-23 07:37:52 -0700

    Add script to pull Apps repo

---
+++ git_apps_xfce4.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+# ---------------------------------------------------------------------- #
+#
+# Name: git_apps_xfce4.py
+# Purpose: Pulls Xfce4 git apps code from GitLab repositories
+#
+#
+# version: 0.1
+# updated: 20200523
+# @author: kevin.bowen@gmail.com
+#
+# ---------------------------------------------------------------------- #
+
+import os
+
+
+panel_list = ['xfce-screensaver', 'xfmpc', 'xfdashboard',
+              'xfce4-volumed-pulse', 'xfce4-terminal', 'xfce4-taskmanager',
+              'xfce4-screenshooter', 'xfce4-panel-profiles',
+              'xfce4-notifyd', 'xfce4-mixer', 'xfce4-dict',
+              'xfburn', 'ristretto', 'parole', 'mousepad',
+              'gigalo', 'catfish']
+
+os.mkdir('../apps')
+os.chdir('../apps')
+
+for item in panel_list:
+    os.system('git clone https://gitlab.xfce.org/apps/' + item)
