2aab886358c11c1844f207580df0f78f81fd3978
kevinbowen777 <kevin.bowen@gmail.com>
2021-02-16 00:29:42 -0800

    add clean_xfce_all script

---
+++ clean/clean_xfce_all.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+# {{{ ------------------------------------------------------------------ #
+#
+# Name: clean_xfce_all.py
+# Purpose: clean oll of the Xfce repositories from
+#           https://gitlab.xfce.org/
+#
+# version: 0.1
+# updated: 20210215
+# @author: kevin.bowen@gmail.com
+#
+# }}} ------------------------------------------------------------------ #
+
+
+import os
+import subprocess
+import sys
+
+currentdir = os.path.dirname(os.path.realpath(__file__))
+parentdir = os.path.dirname(currentdir)
+sys.path.append(parentdir)
+os.chdir(currentdir)
+
+
+subprocess.call("clean_xfce_apps.py", shell=True)
+subprocess.call("clean_xfce_bindings.py", shell=True)
+subprocess.call("clean_xfce_core.py", shell=True)
+subprocess.call("clean_xfce_panel_plugins.py", shell=True)
+subprocess.call("clean_xfce_thunar_plugins.py", shell=True)
