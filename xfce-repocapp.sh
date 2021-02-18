76320ff06a302de9a4905da16aef025328daa8e6
kevinbowen777 <kevin.bowen@gmail.com>
2021-02-17 17:01:57 -0800

    add choice selection response phrasing

--- xfce-repocapp.sh
+++ xfce-repocapp.sh
@@ -70,42 +70,50 @@ do
             do
                 case $sub1opt in
                     "Clone apps")
-                        echo "you chose choice 1"
+                        echo
+                        echo "Cloning Xfce apps repositories..."
                         clone/clone_xfce_apps.py
                         exit
                         ;;
                     "Clone bindings")
-                        echo "you chose choice 2"
+                        echo
+                        echo "Cloning Xfce bindings repositories..."
                         clone/clone_xfce_bindings.py
                         exit
                         ;;
                     "Clone core")
-                        echo "you chose choice 3"
+                        echo
+                        echo "Cloning Xfce core repositories..."
                         clone/clone_xfce_core.py
                         exit
                         ;;
                     "Clone panel-plugins")
-                        echo "you chose choice 4"
+                        echo
+                        echo "Cloning Xfce panel-plugin repositories..."
                         clone/clone_xfce_panel_plugins.py
                         exit
                         ;;
                     "Clone thunar-plugins")
-                        echo "you chose choice 5"
+                        echo
+                        echo "Cloning Xfce thunar-plugin repositories..."
                         clone/clone_xfce_thunar_plugins.py
                         exit
                         ;;
                     "Clone www")
-                        echo "you chose choice 6"
+                        echo
+                        echo "Cloning Xfce www repositories..."
                         clone/clone_xfce_www.py
                         exit
                         ;;
                     "Clone all repos")
-                        echo "you chose choice 7"
+                        echo
+                        echo "Cloning all Xfce repositories..."
                         clone/clone_xfce_all.py
                         exit
                         ;;
                     "Back to Main Menu")
-                        echo "you chose choice 8"
+                        echo
+                        echo "Returning to Main Menu..."
                         exec "$0"
                         ;;
                     "Quit")
@@ -122,37 +130,44 @@ do
             do
                 case $sub2opt in
                     "Build apps")
-                        echo "you chose choice 1"
+                        echo
+                        echo "Building Xfce apps repositories..."
                         build/build_xfce_apps.py
                         exit
                         ;;
                     "Build bindings")
-                        echo "you chose choice 2"
+                        echo
+                        echo "Building Xfce bindings repositories..."
                         build/build_xfce_bindings.py
                         exit
                         ;;
                     "Build core")
-                        echo "you chose choice 3"
+                        echo
+                        echo "Building Xfce core repositories..."
                         build/build_xfce_core.py
                         exit
                         ;;
                     "Build panel-plugins")
-                        echo "you chose choice 4"
+                        echo
+                        echo "Building Xfce panel-plugin repositories..."
                         build/build_xfce_panel_plugins.py
                         exit
                         ;;
                     "Build thunar-plugins")
-                        echo "you chose choice 5"
+                        echo
+                        echo "Building Xfce thunar-plugin repositories..."
                         build/build_xfce_thunar_plugins.py
                         exit
                         ;;
                     "Build all repos")
-                        echo "you chose choice 6"
+                        echo
+                        echo "Building all Xfce repositories..."
                         build/build_xfce_all.py
                         exit
                         ;;
                     "Back to Main Menu")
-                        echo "you chose choice 7"
+                        echo
+                        echo "Returning to Main Menu..."
                         exec "$0"
                         ;;
                     "Quit")
@@ -169,37 +184,44 @@ do
             do
                 case $sub3opt in
                     "Install apps")
-                        echo "you chose choice 1"
+                        echo
+                        echo "Cleaning Xfce apps repositories..."
                         install/install_xfce_apps.py
                         exit
                         ;;
                     "Install bindings")
-                        echo "you chose choice 2"
+                        echo
+                        echo "Cleaning Xfce bindings repositories..."
                         install/install_xfce_bindings.py
                         exit
                         ;;
                     "Install core")
-                        echo "you chose choice 3"
+                        echo
+                        echo "Cleaning Xfce core repositories..."
                         install/install_xfce_core.py
                         exit
                         ;;
                     "Install panel-plugins")
-                        echo "you chose choice 4"
+                        echo
+                        echo "Cleaning Xfce panel-plugin repositories..."
                         install/install_xfce_panel_plugins.py
                         exit
                         ;;
                     "Install thunar-plugins")
-                        echo "you chose choice 5"
+                        echo
+                        echo "Cleaning Xfce thunar-plugin repositories..."
                         install/install_xfce_thunar_plugins.py
                         exit
                         ;;
                     "Install all repos")
-                        echo "you chose choice 6"
+                        echo
+                        echo "Installing all Xfce repositories..."
                         install/install_xfce_all.py
                         exit
                         ;;
                     "Back to Main Menu")
-                        echo "you chose choice 7"
+                        echo
+                        echo "Returning to Main Menu..."
                         exec "$0"
                         ;;
                     "Quit")
@@ -216,37 +238,44 @@ do
             do
                 case $sub4opt in
                     "Clean apps")
-                        echo "you chose choice 1"
+                        echo
+                        echo "Cleaning Xfce apps repositories..."
                         clean/clean_xfce_apps.py
                         exit
                         ;;
                     "Clean bindings")
-                        echo "you chose choice 2"
+                        echo
+                        echo "Cleaning Xfce bindings repositories..."
                         clean/clean_xfce_bindings.py
                         exit
                         ;;
                     "Clean core")
-                        echo "you chose choice 3"
+                        echo
+                        echo "Cleaning Xfce core repositories..."
                         clean/clean_xfce_core.py
                         exit
                         ;;
                     "Clean panel-plugins")
-                        echo "you chose choice 4"
+                        echo
+                        echo "Cleaning Xfce panel-plugin repositories..."
                         clean/clean_xfce_panel_plugins.py
                         exit
                         ;;
                     "Clean thunar-plugins")
-                        echo "you chose choice 5"
+                        echo
+                        echo "Cleaning Xfce thunar-plugin repositories..."
                         clean/clean_xfce_thunar_plugins.py
                         exit
                         ;;
                     "Clean all repos")
-                        echo "you chose choice 6"
+                        echo
+                        echo "Cleaning all Xfce repositories..."
                         clean/clean_xfce_all.py
                         exit
                         ;;
                     "Back to Main Menu")
-                        echo "you chose choice 7"
+                        echo
+                        echo "Returning to Main Menu..."
                         exec "$0"
                         ;;
                     "Quit")
@@ -263,42 +292,50 @@ do
             do
                 case $sub5opt in
                     "Pull apps")
-                        echo "you chose choice 1"
+                        echo
+                        echo "Pulling Xfce apps repositories..."
                         pull/pull_xfce_apps.py
                         exit
                         ;;
                     "Pull bindings")
-                        echo "you chose choice 2"
+                        echo
+                        echo "Pulling Xfce bindings repositories..."
                         pull/pull_xfce_bindings.py
                         exit
                         ;;
                     "Pull core")
-                        echo "you chose choice 3"
+                        echo
+                        echo "Pulling Xfce core repositories..."
                         pull/pull_xfce_core.py
                         exit
                         ;;
                     "Pull panel-plugins")
-                        echo "you chose choice 4"
+                        echo
+                        echo "Pulling Xfce panel-plugin repositories..."
                         pull/pull_xfce_panel_plugins.py
                         exit
                         ;;
                     "Pull thunar-plugins")
-                        echo "you chose choice 5"
+                        echo
+                        echo "Pulling Xfce thunar-plugin repositories..."
                         pull/pull_xfce_thunar_plugins.py
                         exit
                         ;;
                     "Pull www")
-                        echo "you chose choice 6"
+                        echo
+                        echo "Pulling Xfce www repositories..."
                         pull/pull_xfce_www.py
                         exit
                         ;;
                     "Pull all repos")
-                        echo "you chose choice 7"
+                        echo
+                        echo "Pulling all Xfce repositories..."
                         pull/pull_xfce_all.py
                         exit
                         ;;
                     "Back to Main Menu")
-                        echo "you chose choice 8"
+                        echo
+                        echo "Returning to Main Menu..."
                         exec "$0"
                         ;;
                     "Quit")
@@ -315,42 +352,50 @@ do
             do
                 case $sub6opt in
                     "Purge apps")
-                        echo "you chose choice 1"
+                        echo
+                        echo "Purging Xfce apps repositories..."
                         purge/purge_xfce_apps.py
                         exit
                         ;;
                     "Purge bindings")
-                        echo "you chose choice 2"
+                        echo
+                        echo "Purging Xfce bindings repositories..."
                         purge/purge_xfce_bindings.py
                         exit
                         ;;
                     "Purge core")
-                        echo "you chose choice 3"
+                        echo
+                        echo "Purging Xfce core repositories..."
                         purge/purge_xfce_core.py
                         exit
                         ;;
                     "Purge panel-plugins")
-                        echo "you chose choice 4"
+                        echo
+                        echo "Purging Xfce panel-plugin repositories..."
                         purge/purge_xfce_panel_plugins.py
                         exit
                         ;;
                     "Purge thunar-plugins")
-                        echo "you chose choice 5"
+                        echo
+                        echo "Purging Xfce thunar-plugin repositories..."
                         purge/purge_xfce_thunar_plugins.py
                         exit
                         ;;
                     "Purge www")
-                        echo "you chose choice 6"
+                        echo
+                        echo "Purging Xfce www repositories..."
                         purge/purge_xfce_www.py
                         exit
                         ;;
                     "Purge all repos")
-                        echo "you chose choice 7"
+                        echo
+                        echo "Purging all Xfce repositories..."
                         purge/purge_xfce_all.py
                         exit
                         ;;
                     "Back to Main Menu")
-                        echo "you chose choice 8"
+                        echo
+                        echo "Returning to Main Menu..."
                         exec "$0"
                         ;;
                     "Quit")
