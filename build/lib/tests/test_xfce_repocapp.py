6df773827873893cfb28a58f07e2cf565e70826b
Kevin Bowen <kevin.bowen@gmail.com>
2021-12-28 20:14:35 -0800

    add wheel bdist & sdist files for testing

---
+++ build/lib/tests/test_xfce_repocapp.py
@@ -0,0 +1,10 @@
+from xfce_repocapp import __version__
+from xfce_repocapp import __author__
+
+
+def test_author():
+    assert __author__ == 'Kevin Bowen <kevin.bowen@gmail.com>'
+
+
+def test_version():
+    assert __version__ == '0.8.3'
