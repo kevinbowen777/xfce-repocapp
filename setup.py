edd3a2f40e06e2cb960d27866918a891eb98da52
Kevin Bowen <kevin.bowen@gmail.com>
2021-12-28 20:04:03 -0800

    create setup.py
    (cherry picked from commit 31155a149e1f8c8780ea349b296ef33a8097d17f)

---
+++ setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="xfce-repocapp-KEVIN-BOWEN",
+    version="0.8.3",
+    author="Kevin Bowen",
+    author_email="kevin.bowen@gmail.com",
+    description="Build scripts for managing Xfce repositories",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gitlab.com/kevinbowen/xfce-repocapp",
+    project_urls={
+        "Bug Tracker": "https://gitlab.com/kevinbowen/xfce-repocapp/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+        'Operating System :: POSIX :: Linux',
+        'Development Status :: 3 - Alpha',
+        'Topic :: Software Development :: Build Tools',
+        'Topic :: Software Development :: Version Control',
+        'Topic :: Utilities',
+    ],
+    package_dir={},
+    packages=setuptools.find_packages(),
+    python_requires=">=3.8",
+)
