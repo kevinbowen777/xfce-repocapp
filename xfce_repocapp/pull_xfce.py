36bb36ef033bcb246d67e389ed794e61a4a8c658
Kevin Bowen <kevin.bowen@gmail.com>
2023-03-14 13:27:13 -0700

    Replace os.path with pathlib.Path. #1

--- xfce_repocapp/pull_xfce.py
+++ xfce_repocapp/pull_xfce.py
@@ -6,14 +6,15 @@ Purpose: update local Xfce repositories pulled from
            https://gitlab.xfce.org
 
 source: https://gitlab.com/kevinbowen/xfce-repocapp
-version: 0.8.6
-updated: 20220113
+version: 0.8.7
+updated: 20230314
 @author: kevin.bowen@gmail.com
 """
 
 import argparse
 import os
 import sys
+from pathlib import Path
 
 from cappdata import component_list
 
@@ -37,7 +38,7 @@ parser.add_argument(
     help="specify an Xfce component group to pull/update"
     " from https://gitlab.xfce.org.",
 )
-parser.add_argument("--version", action="version", version="%(prog)s 0.8.6")
+parser.add_argument("--version", action="version", version="%(prog)s 0.8.7")
 args = parser.parse_args()
 if args.component is None:
     print(
@@ -50,23 +51,22 @@ if args.component is None:
 def pull_xfce(component, comp_list):
     """Run 'git pull' on selected components to update repositories."""
     print(f"Updating the Xfce {component} group...")
-    os.chdir(os.path.dirname(os.path.realpath(__file__)))
+    os.chdir(Path(__file__).parent.resolve())
 
     def get_path(comp_group):
         # grandparent directory (../../) relative to script.
-        installpath = os.path.abspath(
-            os.path.join(os.getcwd(), os.pardir, os.pardir, comp_group)
-        )
+        installpath = Path.cwd().parent.parent.joinpath(comp_group)
 
         return installpath
 
     repopath = get_path(component)
     success_count = 0
 
-    if os.path.isdir(repopath):
+    if Path.is_dir(repopath):
         os.chdir(repopath)
         for item in component_list(comp_list):
-            if os.path.isdir(item):
+            p = Path(item)
+            if p.is_dir():
                 os.chdir(item)
                 print(f"Updating {item}...")
                 os.system("git pull")
