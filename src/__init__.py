1f77cf56c82e51fbed35f4ec56a9ae16cf7c6254
Kevin Bowen <kevin.bowen@gmail.com>
2023-03-14 20:43:05 -0700

    Update version & releasedate

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,4 @@
 __author__ = "Kevin Bowen <kevin.bowen@gmail.com>"
-__version__ = "0.8.6"
+__version__ = "0.8.7"
 __doc__ = "A collection of scripts to maintain local Xfce repositories"
-__releasedate__ = "20220116"
+__releasedate__ = "20230314"
